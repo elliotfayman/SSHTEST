4413da2ca27464dc86c4e72f6cbc3189b67549a1
Elliot Fayman <elliotfayman@gmail.com>
2021-09-05 03:51:39 -0700

    first commit

---
+++ testClass.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+public class testClass{
+    public static void main(String args[])
+    {
+        JFrame frame = new JFrame("GUI TIME!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button);
+        frame.setVisible(true);
+
+    }
+        
+
+}
